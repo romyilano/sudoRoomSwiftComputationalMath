f019398c68f8aec676c1962bca1f235478580184
romy <romy@snowyla.com>
2024-08-05 20:15:09 -0700

    Initial Commit

---
+++ NumericsStuff/NumericsStuffApp.swift
@@ -0,0 +1,17 @@
+//
+//  NumericsStuffApp.swift
+//  NumericsStuff
+//
+//  Created by Romy Ilano on 8/5/24.
+//
+
+import SwiftUI
+
+@main
+struct NumericsStuffApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NumericsStuffTests/NumericsStuffTests.swift
@@ -0,0 +1,16 @@
+//
+//  NumericsStuffTests.swift
+//  NumericsStuffTests
+//
+//  Created by Romy Ilano on 8/5/24.
+//
+
+import Testing
+
+struct NumericsStuffTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
